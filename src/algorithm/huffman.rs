724b59a8ff7337439c4406d5b5738574885b38bd
melcar <15875473+melcar@users.noreply.github.com>
2024-02-18 18:08:54 +0100

    added Frequency enum and tests covering frequency calls

--- src/algorithm/huffman.rs
+++ src/algorithm/huffman.rs
@@ -1,14 +1,34 @@
+use super::util::binary_tree::Tree;
+use std::{collections::BTreeMap, ops::Add};
+
+#[derive(PartialEq, PartialOrd, Ord, Eq, Debug)]
 pub struct Frequency {
-    character: char,
-    frequency: i32,
+    pub frequency: u16, // frequency is a value between 0 and 65536 and is equal to n/65536
+    pub character: char,
+}
+
+fn build_huffman_tree(frequencies: Vec<Frequency>) -> Tree<Frequency> {
+    todo!()
 }
 
 pub enum Huffman {
     Encoded {
-        frequencies: Vec<Frequency>,
+        frequencies: Tree<Frequency>,
         encoded: String,
     },
     Decoded(String),
+}
+
+pub fn compute_frequencies(data: &str) -> Vec<Frequency> {
+    let mut char_counts: BTreeMap<char, usize> = BTreeMap::new();
+    data.chars().for_each(|c| {
+        char_counts.entry(c).and_modify(|n| *n += 1).or_insert(1);
+    });
+
+    Vec::from_iter(char_counts.into_iter().map(|(c, f)| Frequency {
+        frequency: { (((f as f64) / (data.len() as f64)) * (std::u16::MAX as f64)) as u16 },
+        character: c,
+    }))
 }
 
 pub fn compress(data: &str) -> crate::huffman::Huffman {
--- tests/huffman_tests.rs
+++ tests/huffman_tests.rs
@@ -1,6 +1,121 @@
-use cmprsd::algorithm::huffman;
+use rand::{distributions::Alphanumeric, thread_rng, Rng};
+
+use cmprsd::algorithm::huffman::{self, Frequency};
+
+const LOREM_IPSUM : &str ="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+const EPSILON: f64 = 0.00001_f64;
+//make a macro out of it
+fn close_to(a: f64, b: f64, delta: f64) -> bool {
+    a - b < delta
+}
 
 #[test]
 pub fn huffman_empty_string() {
     let compressed = huffman::compress("");
+}
+
+#[test]
+pub fn huffman_repeating_string() {
+    let compressed = huffman::compress("");
+}
+
+#[test]
+pub fn huffman_two_characters() {
+    let compressed = huffman::compress("");
+}
+
+#[test]
+pub fn huffman_random() {
+    let compressed = huffman::compress("");
+}
+
+#[test]
+pub fn huffman_lorem() {
+    let compressed = huffman::compress(LOREM_IPSUM);
+}
+
+#[test]
+pub fn compute_frequencies_empty_string() {
+    assert!(huffman::compute_frequencies("").is_empty());
+}
+
+#[test]
+pub fn compute_frequencies_one_character() {
+    assert_eq!(
+        huffman::compute_frequencies("a")[0],
+        huffman::Frequency {
+            character: 'a',
+            frequency: std::u16::MAX
+        }
+    )
+}
+
+#[test]
+pub fn compute_frequencies_two_character_50_50() {
+    let frequencies = huffman::compute_frequencies("abab");
+    assert_eq!(
+        frequencies[0],
+        huffman::Frequency {
+            character: 'a',
+            frequency: std::u16::MAX / 2
+        },
+    );
+    assert_eq!(
+        frequencies[1],
+        huffman::Frequency {
+            character: 'b',
+            frequency: std::u16::MAX / 2
+        },
+    );
+}
+
+#[test]
+pub fn compute_frequencies_two_character_25_75() {
+    let frequencies = huffman::compute_frequencies("bbab");
+    assert_eq!(
+        frequencies[0],
+        huffman::Frequency {
+            character: 'a',
+            frequency: std::u16::MAX / 4
+        },
+    );
+    assert_eq!(
+        frequencies[1],
+        huffman::Frequency {
+            character: 'b',
+            frequency: 3 * (std::u16::MAX / 4)
+        },
+    );
+}
+
+fn check_frequencies(frequencies: Vec<Frequency>, string: &str) {
+    for frequency in frequencies.iter() {
+        assert!(close_to(
+            (frequency.frequency as f64) / (std::u16::MAX as f64),
+            string.chars().filter(|&c| c == frequency.character).count() as f64
+                / string.len() as f64,
+            EPSILON
+        ));
+    }
+}
+
+#[test]
+pub fn compute_frequencies_lorem_ipsum() {
+    check_frequencies(huffman::compute_frequencies(LOREM_IPSUM), LOREM_IPSUM);
+}
+
+#[test]
+pub fn compute_frequencies_random_long_string() {
+    let mut rng = thread_rng();
+
+    let string_length: usize = rng.gen_range(500_000..1_000_000);
+
+    let random_string: String = (&mut rng)
+        .sample_iter(Alphanumeric)
+        .take(string_length)
+        .map(char::from)
+        .collect();
+
+    check_frequencies(huffman::compute_frequencies(&random_string), &random_string)
 }
