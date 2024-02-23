019051152979bd45bcadf664a3d354c28d918e50
melcar <15875473+melcar@users.noreply.github.com>
2024-02-24 00:02:50 +0100

    added bfs traversal + a few tests

--- src/algorithm/util/binary_tree.rs
+++ src/algorithm/util/binary_tree.rs
@@ -1,16 +1,16 @@
 //Naive binary tree implementation
-
+use std::{collections::VecDeque, ops::Deref};
 #[derive(PartialEq, PartialOrd, Ord, Eq, Debug, Clone)]
-pub enum Tree<T: std::cmp::Ord> {
+pub enum Tree<T: std::cmp::Ord + Copy> {
     Node {
         content: T,
-        left: Box<Tree<T>>,
+        left: Box<Tree<T>>,//should be optionals
         right: Box<Tree<T>>,
     },
     Leaf(T),
 }
 
-impl<T: std::cmp::Ord> Tree<T> {
+impl<T: std::cmp::Ord + Copy> Tree<T> {
     pub fn len(&self) -> usize {
         match self {
             Tree::Leaf(_) => 1,
@@ -26,8 +26,30 @@ impl<T: std::cmp::Ord> Tree<T> {
         self.len() == 0
     }
 
-    pub fn to_breadth_first_search() {
-        
+    pub fn to_breadth_first_search(&self) -> Vec<T> {
+        let mut visited_nodes: VecDeque<&Tree<T>> = VecDeque::new();
+        let mut bfs_nodes: Vec<T> = Vec::new();
+        visited_nodes.push_back(self);
+        while !visited_nodes.is_empty() {
+            let node = visited_nodes
+                .pop_front()
+                .expect("can't be empty at this point");
+
+            match node {
+                Tree::Node {
+                    content: _,
+                    left,
+                    right,
+                } => {
+                    visited_nodes.push_back(left);
+                    visited_nodes.push_back(right)
+                }
+                _ => (),
+            }
+
+            bfs_nodes.push(*node.get_value());
+        }
+        bfs_nodes
     }
 
     pub fn build_internal_node(content: T, left: Tree<T>, right: Tree<T>) -> Tree<T> {
---
+++ tests/algorithm/util/binary_tree.rs
@@ -0,0 +1,69 @@
+use cmprsd::algorithm::util::binary_tree::Tree;
+
+fn compare_bfs<T: std::cmp::Ord>(bfs: Vec<T>, expected_bfs: Vec<T>) {
+    assert_eq!(bfs.len(), expected_bfs.len());
+    bfs.iter()
+        .zip(expected_bfs.iter())
+        .all(|(result, expected)| result == expected);
+}
+
+#[test]
+pub fn binary_tree_bfs_1_node() {
+    let tree = Tree::<u16>::Leaf(0);
+    let bfs = tree.to_breadth_first_search();
+    let expected_bfs: Vec<u16> = vec![1];
+}
+
+#[test]
+pub fn binary_tree_bfs_3_node() {
+    let left_node = Tree::<u16>::Leaf(1);
+    let right_node = Tree::<u16>::Leaf(2);
+    let tree = Tree::<u16>::Node {
+        content: 0,
+        left: Box::new(left_node),
+        right: Box::new(right_node),
+    };
+    let bfs = tree.to_breadth_first_search();
+    let expected_bfs: Vec<u16> = vec![0,1,2];
+    compare_bfs(bfs, expected_bfs)
+}
+
+#[test]
+pub fn binary_tree_bfs_4_node() {
+    let l1_left_node = Tree::<u16>::Leaf(3);
+    let l1_right_node = Tree::<u16>::Leaf(4);
+    let l0_left_node = Tree::<u16>::Node {
+        content: 1,
+        left: Box::new(l1_left_node),
+        right: Box::new(l1_right_node),
+    };
+    let l0_right_node = Tree::<u16>::Leaf(2);
+    let root = Tree::<u16>::Node{
+        content: 0,
+        left: Box::new(l0_left_node),
+        right: Box::new(l0_right_node)
+    };
+    let bfs =root.to_breadth_first_search();
+    let expected_bfs: Vec<u16> = vec![0,1,2,3,4];
+    compare_bfs(bfs, expected_bfs)
+}
+
+#[test]
+pub fn binary_tree_bfs_4_node_symmetric() {
+    let l1_left_node = Tree::<u16>::Leaf(3);
+    let l1_right_node = Tree::<u16>::Leaf(4);
+    let l0_right_node = Tree::<u16>::Node {
+        content: 1,
+        left: Box::new(l1_left_node),
+        right: Box::new(l1_right_node),
+    };
+    let l0_left_node = Tree::<u16>::Leaf(2);
+    let root = Tree::<u16>::Node{
+        content: 0,
+        left: Box::new(l0_left_node),
+        right: Box::new(l0_right_node)
+    };
+    let bfs =root.to_breadth_first_search();
+    let expected_bfs: Vec<u16> = vec![0,2,1,3,4];
+    compare_bfs(bfs, expected_bfs)
+}
