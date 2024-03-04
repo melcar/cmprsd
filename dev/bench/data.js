3810477cb20ec2fefcf1927f459b9b8b08eca1dc
github-action-benchmark <github@users.noreply.github.com>
2024-03-04 17:29:01 +0000

    add Rust Benchmark (cargo) benchmark result for 4291ab3903310cc4e3ee3ebb386a7af040c4dde9

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1709573341531,
+  "repoUrl": "https://github.com/melcar/cmprsd",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "15875473+melcar@users.noreply.github.com",
+            "name": "melcar",
+            "username": "melcar"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4291ab3903310cc4e3ee3ebb386a7af040c4dde9",
+          "message": "Update README.md",
+          "timestamp": "2024-03-04T18:26:43+01:00",
+          "tree_id": "f02bba32cfde573d3b85d2d12b58040932c192ac",
+          "url": "https://github.com/melcar/cmprsd/commit/4291ab3903310cc4e3ee3ebb386a7af040c4dde9"
+        },
+        "date": 1709573340953,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "100 samples/Decompressing Hello worlds",
+            "value": 1611,
+            "range": "± 3",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
